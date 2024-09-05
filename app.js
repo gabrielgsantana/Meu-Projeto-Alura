d73ccce0ac53444b444a7d2caed1089adcd7cb85
Gabriel Santana <122163924+gabrielgsantana@users.noreply.github.com>
2024-09-05 16:47:36 -0300

    a

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-console.log(dados);
-
-
-
-
-
-
-
-/*Função para criar e mostrar a notificação*/
-function showNotification(message) {
-    const notification = document.createElement('div'); /*Cria um elemento div para a notificação*/
-    notification.className = 'notification';
-    notification.innerText = message; /*Adiciona a notificação ao corpo do documento*/
-    document.body.appendChild(notification);
-    setTimeout(() => { notification.remove(); }, 4000); /*Remove a notificação após 5 segundos*/
-}
-
-/*Seleciona o botão de pesquisa*/
-const searchButton = document.querySelector('section button');
-
-/* Adiciona um evento de clique ao botão, se ele existir */
-if (searchButton) {
-    searchButton.addEventListener('click', () => {
-        showNotification('Parabéns, Você Conseguiu!');
-    });
-}
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let resultados = "";
+  
+    // Itera sobre cada dado da pesquisa
+    for (let dado of dados) {
+      // Constrói o HTML para um item de resultado
+      resultados += `
+        <div class="item-resultado">
+          <h2><a href="#">${dado.titulo}</a></h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a target="_blank" href=${dado.link}>Mais Informações</a>
+        </div>
+      `;
+    }
+  
+    // Atualiza o conteúdo da seção com os resultados construídos
+    section.innerHTML = resultados;
+  }
 
