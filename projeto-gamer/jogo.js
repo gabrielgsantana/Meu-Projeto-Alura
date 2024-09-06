1fbd4661cf4ee524552efe8299e54d08b432d05d
Gabriel Santana <122163924+gabrielgsantana@users.noreply.github.com>
2024-09-06 17:39:01 -0300

    a

--- projeto-gamer/jogo.js
+++ projeto-gamer/jogo.js
@@ -1,41 +1,37 @@
 const coala = document.querySelector('.coala');
 const obstaculo = document.querySelector('.obstaculo');
-
+const nuvens = document.querySelectorAll('.nuvem'); // Seleciona todas as nuvens
 
 const pulo = () => {
-    coala.classList.add('pulo')
+  coala.classList.add('pulo');
 
-    setTimeout(() => {
-
-        coala.classList.remove('pulo');
-
-    }, 500);
+  setTimeout(() => {
+    coala.classList.remove('pulo');
+  }, 500);
 }
 
 const loop = setInterval(() => {
+  console.log('loop');
 
-    console.log('loop');
+  const obstaculoPosition = obstaculo.offsetLeft;
+  const coalaPosition = +window.getComputedStyle(coala).bottom.replace('px', '');
 
-   const obstaculoPosition = obstaculo.offsetLeft;
-   const coalaPosition = +window.getComputedStyle(coala) .bottom.replace('px', '');
-   
-   if (obstaculoPosition <= 115 && obstaculoPosition > 0 && coalaPosition < 95 )  {
-
+  if (obstaculoPosition <= 115 && obstaculoPosition > 0 && coalaPosition < 95) {
     obstaculo.style.animation = 'none';
     obstaculo.style.left = `${obstaculoPosition}px`;
 
-    coala.style.animation = 'none';
-    coala.style.bottom = `${coalaPosition}px`;
+    // Para todas as nuvens:
+    nuvens.forEach(nuvem => {
+      nuvem.style.animation = 'none'; // Para a animação da nuvem
+    });
 
     coala.src = "./imagens/game-over.png";
-    coala.style.width = '130px'
-    coala.style.marginLeft = '23px'
-    coala.style.marginBottom = '-9px'
+    coala.style.width = '130px';
+    coala.style.marginLeft = '23px';
+    coala.style.marginBottom = '-9px';
 
-    clearInterval(loop)
+    clearInterval(loop);
+  }
+}, 10);
 
-   }
-   
-},10);
-
-document.addEventListener('keydown', pulo)
+document.addEventListener('keydown', pulo);
