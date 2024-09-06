97a5964ffa767a4d11392dc651f93b830c4ab119
Gabriel Santana <122163924+gabrielgsantana@users.noreply.github.com>
2024-09-06 15:13:25 -0300

    adicionei o novo fundo e mudei o javascrpit

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 function pesquisar() {
-    // Seleciona a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-  
-    // Inicializa uma string vazia para armazenar o HTML dos resultados
-    let resultados = "";
-  
-    // Itera sobre cada dado da pesquisa
-    for (let dado of dados) {
-      // Constrói o HTML para um item de resultado
-      resultados += `
-        <div class="item-resultado">
-          <h2><a href="#">${dado.titulo}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a target="_blank" href=${dado.link}>Mais Informações</a>
-        </div>
-      `;
-    }
-  
-    // Atualiza o conteúdo da seção com os resultados construídos
-    section.innerHTML = resultados;
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  //se campoPesquisa for uma string sem nada
+
+  if(!campoPesquisa){
+    section.innerHTML = "<p> Nada foi encontrado. Você precisar digitar alguma informação! <p/>"
+      return
   }
 
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // Inicializa uma string vazia para armazenar o HTML dos resultados
+  let resultados = "";
+  let titulo =  "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada dado da pesquisa
+  for (let dado of dados) {
+
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+
+    //se titulo includes campoPesquisa
+    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+       // Constrói o HTML para um item de resultado
+      resultados += `
+      <div class="item-resultado">
+        <h2><a href="#">${dado.titulo}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a target="_blank" href=${dado.link}>Mais Informações</a>
+      </div>
+    `;
+
+    }
+   }
+
+   if (!resultados){
+        resultados = "<p> Nada foi encontrado </p>"
+   }
+
+  // Atualiza o conteúdo da seção com os resultados construídos
+  section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -3,31 +3,37 @@ let dados = [
         titulo: "Coala",
         descricao: "O coala (*Phascolarctos cinereus*) é um marsupial australiano conhecido por sua pelagem cinza e orelhas grandes. Vivendo em árvores de eucalipto, torna-se um símbolo importante da conservação ambiental na Austrália.",
         link: "https://pt.wikipedia.org/wiki/Coala",
+        tags:"coalas koala koalas colas cola",
     },
     {
         titulo: "Habitat",
         descricao: "O coala habita as florestas de eucaliptos da Austrália, principalmente nas regiões costeiras do leste e sul do continente. A escolha das árvores é crucial, pois a dieta do coala é baseada exclusivamente em folhas de eucalipto.",
-        link: "https://pt.wikipedia.org/wiki/Coala#Distribui%C3%A7%C3%A3o_geogr%C3%A1fica_e_habitat"
+        link: "https://pt.wikipedia.org/wiki/Coala#Distribui%C3%A7%C3%A3o_geogr%C3%A1fica_e_habitat",
+        tags:"eco sistema ecossistema meio ambiente lugar mora vive onde o coala vive",
     },
     {
         titulo: "Predadores",
         descricao: "Os principais predadores naturais do coala são aves de rapina, como águias e corujas. Dingoes e gatos selvagens também podem representar uma ameaça, especialmente para filhotes. No entanto, a maior ameaça aos coalas hoje em dia são as atividades humanas, como a perda de habitat e os atropelamentos.",
-        link: "https://pt.wikipedia.org/wiki/Coala#Predadores"
+        link: "https://pt.wikipedia.org/wiki/Coala#Predadores",
+        tags:"caçadores caça predador predadora pedrador pedrado caçador caçar",
     },
     {
         titulo: "Etimologia",
         descricao: "A palavra 'koala' tem origem na língua Dharug, falada pelos povos indígenas australianos da região de Sydney. Acredita-se que signifique 'não beber', uma referência ao hábito do coala de obter a maior parte de sua água através das folhas de eucalipto.",
-        link: "https://pt.wikipedia.org/wiki/Coala#Etimologia"
+        link: "https://pt.wikipedia.org/wiki/Coala#Etimologia",
+        tags:"significado nome oque significa o nome coala oq significa o nome coala ",
     },
     {
         titulo: "Alimentação",
         descricao: "A dieta do coala é altamente especializada, consistindo quase exclusivamente de folhas de eucalipto. Devido ao alto teor de toxinas dessas folhas, o metabolismo do coala é extremamente lento, o que lhes permite passar a maior parte do dia dormindo.",
-        link: "https://pt.wikipedia.org/wiki/Coala#Eucalipto"
+        link: "https://pt.wikipedia.org/wiki/Coala#Eucalipto",
+        tags:"comida comer raçao ração árvore arvore arvoré arvóre arvóré árvóré oque o coala come?",
     },
 
     {
         titulo: "Características",
         descricao: "Os coalas são conhecidos por suas características físicas distintivas, como a pelagem densa e macia, as orelhas grandes e arredondadas, e o nariz grande e em forma de colher. São animais solitários e noturnos, e possuem uma bolsa marsupial para carregar seus filhotes.",
-        link: "https://pt.wikipedia.org/wiki/Coala#Caracter%C3%ADsticas"
+        link: "https://pt.wikipedia.org/wiki/Coala#Caracter%C3%ADsticas",
+        tags:"cara rosto fucinho como é o coala?",
     }
 ];
